daa0a8654cfbf17062c317a0175fda7dd38c9e4a
khaykay <berryscott97@gmail.com>
2023-03-09 16:01:25 +0100

    toggle effect

---
+++ QR-code-component/Toggle.js
@@ -0,0 +1,17 @@
+let container = document.getElementsByClassName("container");
+let card = document.getElementsByClassName("card");
+let main = document.getElementsByTagName("main");
+let slider = document.getElementsByClassName("slider");
+// let checkbox = document.getElementById("checkbox");
+const toggle = () => {
+  if (slider.style.backgroundColor === "transparent") {
+    //   container.style.width = "100%";
+    //   container.style.height = "100%";
+    //   container.style.boxShadow = "none";
+    //   main.style.backgroundColor = "var(--Light-gray)";
+    //   main.style.gap = "0";
+    //   card.style.height = "85%";
+    //   card.style.width = "90%";
+    console.log("i work");
+  }
+};
