41ff4f8589e19bf16c047c7d4d10c838bc31ac0c
khaykay <berryscott97@gmail.com>
2023-03-10 17:30:05 +0100

    file update

--- QR-code-component/Toggle.js
+++ QR-code-component/Toggle.js
@@ -4,14 +4,13 @@ let main = document.getElementsByTagName("main");
 let slider = document.getElementsByClassName("slider");
 // let checkbox = document.getElementById("checkbox");
 const toggle = () => {
-  if (slider.style.backgroundColor === "transparent") {
-    //   container.style.width = "100%";
-    //   container.style.height = "100%";
-    //   container.style.boxShadow = "none";
-    //   main.style.backgroundColor = "var(--Light-gray)";
-    //   main.style.gap = "0";
-    //   card.style.height = "85%";
-    //   card.style.width = "90%";
-    console.log("i work");
-  }
+  container.style.width = "100%";
+  container.style.height = "100%";
+  container.style.boxShadow = "none";
+  main.style.backgroundColor = "var(--Light-gray)";
+  main.style.gap = "0";
+  card.style.height = "85%";
+  card.style.width = "90%";
+  // console.log("i work");
+  // console.log(slider);
 };
