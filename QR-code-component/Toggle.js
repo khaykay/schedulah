12917866b19f9495b63cd674f698c645c44f6d2f
khaykay <berryscott97@gmail.com>
2023-03-13 19:57:20 +0100

    toggle button update

--- QR-code-component/Toggle.js
+++ QR-code-component/Toggle.js
@@ -1,34 +1,20 @@
-// let container = document.getElementsByClassName("container");
-// let card = document.getElementsByClassName("card");
-
-// let slider = document.getElementsByClassName("slider");
-// slider.addEventListener("click", function (event) {
-//   let main = document.getElementsByTagName("main");
-
-//   main.style.backgroundColor = "coral";
-
-//   // 👇️ optionally change text color
-//   // box.style.color = 'white';
-// });
-
+const toggle = () => {
+  // let container = document.getElementsByClassName("container");
+  // let card = document.getElementsByClassName("card");
+  let main = document.getElementsByTagName("card");
+  // let slider = document.getElementsByClassName("slider");
+  // container.style.width = "100%";
+  // container.style.height = "100%";
+  // container.style.boxShadow = "none";
+  main.style.backgroundColor = "white";
+  // main.style.gap = "0";
+  // card.style.height = "85%";
+  // card.style.width = "90%";
+  console.log("i work");
+  // console.log(container.style);
+};
 const clicked = () => {
   console.log("clicked");
   const btn = document.getElementById("btn");
   btn.style.backgroundColor = "coral";
 };
-
-// 👇️ optionally change text color
-// box.style.color = "white";
-
-// let checkbox = document.getElementById("checkbox");
-// const toggle = (e) => {
-//   container.style.width = "100%";
-//   container.style.height = "100%";
-//   container.style.boxShadow = "none";
-//   main.style.backgroundColor = "var(--Light-gray)";
-//   main.style.gap = "0";
-//   card.style.height = "85%";
-//   card.style.width = "90%";
-//   console.log("i work");
-//   console.log(container.style);
-// };
